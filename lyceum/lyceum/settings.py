b8ac558b5d591caeb3899583e1dcb0098310c32f
Veluon <117262377+Veluon@users.noreply.github.com>
2023-10-07 00:41:15 +0300

    Add files via upload

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import get_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = '../.env'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!n)b)%j(4o31xm7xltdp=(sw@f9cy2-wiw(jg802zm^8$o+%!d'
+SECRET_KEY = get_variable(env, "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_variable(env, "DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -52,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lyceum.urls'
+ROOT_URLCONF = get_variable(env, "ROOT_URLCONF")
 
 TEMPLATES = [
     {
@@ -70,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lyceum.wsgi.application'
+WSGI_APPLICATION = get_variable(env, "WSGI_APPLICATION")
 
 
 # Database
@@ -106,21 +109,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = get_variable(env, "LANGUAGE_CODE")
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = get_variable(env, "TIME_ZONE")
 
-USE_I18N = True
+USE_I18N = get_variable(env, "USE_I18N")
 
-USE_TZ = True
+USE_TZ = get_variable(env, "USE_TZ")
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = get_variable(env, "STATIC_URL")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = get_variable(env, "DEFAULT_AUTO_FIELD")
